aa4d17368ad327b34efd82b6c2ddcd8ab52d9211
oms064 <otokonoko064@gmail.com>
2015-12-01 23:08:59 -0600

    Cambio en el Golpe para que podamos pasarle el botón que querramos presionar.

--- Assets/Scripts/RightArmBehavior.cs
+++ Assets/Scripts/RightArmBehavior.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class RightArmBehavior : MonoBehaviour {
     private Animator animator;
+    public string input = "w";
 
     void Awake() {
         animator = GetComponent<Animator>();
@@ -14,7 +15,7 @@ public class RightArmBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey("w"))
+        if (Input.GetKey(input))
         {
             animator.SetInteger("Status", 1);
         }
